827916b3879d691345587aba4cc2352832927295
Harshana Batagalla <harshanabatagalla@gmail.com>
2023-02-18 14:38:36 +0530

    headder styled with inline css

--- src/Headder.js
+++ src/Headder.js
@@ -1,8 +1,16 @@
 import React from "react";
 
 const Header = () => {
-    return (
-        <div className="header">
+    const headerStyle = {
+        textAlign:"center",
+        padding:20,
+        background:'#000',
+        color:'#fff',
+        textTransform:'uppercase' 
+        }
+    
+        return (
+        <div style={headerStyle}>
             Phone directory
         </div>
     );

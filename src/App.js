60e8e6038cab6b2aa05d919b0d81b6565b5495ff
Harshana Batagalla <harshanabatagalla@gmail.com>
2023-02-21 22:02:50 +0530

    Added delete btn

--- src/App.js
+++ src/App.js
@@ -1,18 +1,44 @@
 import React from 'react';
 import Header from './Header';
+import './App.css'
 
 function App() {
+
+  let contacts= [
+    {
+      id:1,
+      name:"Harshana Batagalla",
+      phone:"1987465231"
+    },
+    {
+      id:2,
+      name:"Theekshani Poornima",
+      phone:"2369789456"
+    }
+  ];
 
   return (
     <div>
 
       <Header />
-      <button>Add</button>
-
       <div>
+        <button className='button-add'>Add</button>
+      </div>
+
+
+      <div className='input-container'>
         <span> Name </span> <br />
         <span> Phone </span>
       </div>
+    {
+      contacts.map(sub => {
+        return <div key={sub.id}className='contacts'>
+          <span>{sub.name}</span>
+          <span>{sub.phone}</span>
+          <span><button className='button-delete'>Delete</button></span>
+          </div>
+      })
+    }
 
     </div>
 
